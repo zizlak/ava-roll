0f3b5349c6f3827f68ea40b617b9a0fa86b0fe44
Aleksandr Kurdiukov <aleksandr.kurdiukov@blacklane.com>
2026-06-11 07:26:26 +0200

    rename ava-roll

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/roll-and-race/",
+  base: "/ava-roll/",
   server: {
     host: "::",
     port: 8080,
